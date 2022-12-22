55354a56ccab4bf6bb9f9adaad39613c81a8b316
Romulo Portillo <rportillo@acecashexpress.com>
2022-12-22 14:05:23 -0300

    Creating the initial structure for 3-tier project pattern.

---
+++ CompleteMvcApp/src/Dev.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dev.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
